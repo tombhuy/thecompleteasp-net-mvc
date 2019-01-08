28acc79c25ef4df80be5649f33884b4b616b7766
tombhuy <44264315+tombhuy@users.noreply.github.com>
2019-01-08 17:57:49 +0700

    Chương 9 - cài đặt glimpses

--- 4-FirstApplication/4-FirstApplication/Controllers/HomeController.cs
+++ 4-FirstApplication/4-FirstApplication/Controllers/HomeController.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 
 namespace _4_FirstApplication.Controllers
 {
     [AllowAnonymous]
     public class HomeController : Controller
     {
+        [OutputCache(Duration = 0,VaryByParam = "*", NoStore = true)]
+        //[OutputCache(Duration = 0,Location = OutputCacheLocation.Server,VaryByParam = "*")]
         public ActionResult Index()
         {
             return View();
